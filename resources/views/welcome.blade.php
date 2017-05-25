ab5278817442eaa5ab81ada376e9b573514e1ca5
Michael McMullen <michael.git.mcmullen@tutelagesystems.com>
2017-05-24 22:21:43 -0400

    Updated name to pastabin

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,7 +20,7 @@
         <div class="form-group">
             <label for="">Share Link</label>
             <div class="input-group">
-                <input type="text" class="form-control" id="short-url" value="http://zonda-paste.dev/a8s726" readonly />
+                <input type="text" class="form-control" id="short-url" value="http://pasta-bin.dev/a8s726" readonly />
                 <span class="input-group-addon">
                     <a href="#" data-toggle="tooltip" data-placement="left" title="Copy link" class="clipboard" data-clipboard-target="#short-url"><i class="fa fa-fw fa-clipboard"></i></a>
                 </span>
